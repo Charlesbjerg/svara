acb9c655d73cc4d65ece5e99e33c60e75d06c2aa
Charlie Underhill <charlie@charlesbjerg.co.uk>
2020-12-23 17:24:46 +0000

    Added Vue SPA and setup Laravel routes for SPA

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Svara</title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="http://localhost:8080/js/app.js" rel="preload" as="script">
+        <link href="http://localhost:8080/js/chunk-vendors.js" rel="preload" as="script">
+    </head>
+    <body>
+       <div id="app"></div>
+        <script src="http://localhost:8080/js/chunk-vendors.js"></script>
+        <script src="http://localhost:8080/js/app.js"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', [\App\Http\Controllers\ViewController::class, 'app'])->where('any','^(?!api).*$');
+//Route::get('/{any}', 'SpaController@index')->where('any', '.*');
